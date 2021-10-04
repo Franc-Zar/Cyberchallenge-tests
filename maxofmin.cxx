c638dfce6fc4a9e0021b06ae47855b79ab55d8e9
Franc-Zar <s1093617@studenti.univpm.it>
2021-10-04 15:22:50 +0200

    esercizi ammissione cyberchallenge

--- maxofmin.cxx
+++ maxofmin.cxx
@@ -3,62 +3,63 @@
 
 using namespace std;
 
-void solve(int N ,int V[]){
-  
-int sol[N];
-int dim_subsq=1;
-int maxofmin;
-int indice=0;
-int min;
+void solve(int N, int V[])
+{
 
-while(dim_subsq<=N) {
-	
-	int minimi[N-dim_subsq+1];
-	
-	for(int i=0; i<=N-dim_subsq;i++) {
-		
-		 min=V[i];
-		   
-		      for(int j=0; j<dim_subsq; j++) 
-		      if(V[i+j]<min) min=V[i+j];
-		 
-	minimi[i]=min;
+  int sol[N];
+  int dim_subsq = 1;
+  int maxofmin;
+  int indice = 0;
+  int min;
 
-}
+  while (dim_subsq <= N)
+  {
 
-maxofmin=minimi[0];
-  
-for(int i=1; i<N-dim_subsq+1;i++) if(maxofmin<minimi[i]) maxofmin=minimi[i];
+    int minimi[N - dim_subsq + 1];
 
-sol[indice]=maxofmin;
-indice++;
-dim_subsq++;
-}
+    for (int i = 0; i <= N - dim_subsq; i++)
+    {
+      min = V[i];
+      for (int j = 0; j < dim_subsq; j++)
+        if (V[i + j] < min)
+          min = V[i + j];
 
-
-
-
-
-						                         	  
-  
-  for(int i=0; i<N; i++) {
-    std::cout << sol[i];
-    if(i < N-1) {
-      std::cout << " ";
-    } else {
-      std::cout << std::endl;
+      minimi[i] = min;
     }
   }
 }
 
-int main(){
-  int N;
-  cin >> N;
-  //int V[N];
-  int V[N];
-  for(int i=0; i<N; i++) V[i]=rand();
-  solve(N,V);
-  //solve(10000 , V);
-  return 0;
+maxofmin = minimi[0];
+
+for (int i = 1; i < N - dim_subsq + 1; i++)
+  if (maxofmin < minimi[i])
+    maxofmin = minimi[i];
+
+sol[indice] = maxofmin;
+indice++;
+dim_subsq++;
+
+for (int i = 0; i < N; i++)
+{
+  std::cout << sol[i];
+  if (i < N - 1)
+  {
+    std::cout << " ";
+  }
+  else
+  {
+    std::cout << std::endl;
+  }
+}
 }
 
+int main()
+{
+  int N;
+  cin >> N;
+  int V[N];
+  for (int i = 0; i < N; i++)
+    V[i] = rand();
+  solve(N, V);
+  return 0;
+}
--- polynomials.cxx
+++ polynomials.cxx
@@ -1,6 +1,3 @@
-
-
-
 #include <iostream>
 #include <ctime>
 #include <cstdlib>
@@ -8,73 +5,66 @@
 
 using namespace std;
 
-void genera_polinomiale(int ,int ,int[]);
+void genera_polinomiale(int, int, int[]);
 int numero_casuale(int);
-void azzera_polinomiale(int,int,int[]);
-
+void azzera_polinomiale(int, int, int[]);
 
 int main(int argc, char **argv)
-{   srand(time(NULL));
-	int n,k;
-	
-	cout<< "inserisci n e k" << endl;
+{
+	srand(time(NULL));
+	int n, k;
+
+	cout << "inserisci n e k" << endl;
 	cin >> n >> k;
-	
-	int dim=n+1;
+
+	int dim = n + 1;
 	int coefficienti[dim];
-genera_polinomiale(k,dim,coefficienti);
 
-for(int z :coefficienti) cout << z << " "  ;
+	genera_polinomiale(k, dim, coefficienti);
 
+	for (int z : coefficienti)
+		cout << z << " ";
 
-azzera_polinomiale(k,dim,coefficienti);
+	azzera_polinomiale(k, dim, coefficienti);
 
-
-
-
-
-	
 	return 0;
 }
 
-void genera_polinomiale(int k,int n,int x[]) { 
-	
-	 int somma=0;
-	while (somma==0) {
-	
-	for(int i=0; i<n;i++){
-		 x[i]=numero_casuale(k);
-	                    somma+=x[i]*pow(2,i); }
-    
- 
-	
-}}
+void genera_polinomiale(int k, int n, int x[])
+{
 
-int numero_casuale(int k) {
-	
-	return rand() % ((2*k)+1) + (-k);
+	int somma = 0;
+	while (somma == 0)
+	{
+		for (int i = 0; i < n; i++)
+		{
+			x[i] = numero_casuale(k);
+			somma += x[i] * pow(2, i);
+		}
+	}
 }
-	
 
-void azzera_polinomiale(int k,int n, int x[]) {
-	
-	int soluzioni=0;
-	int somma=0;
+int numero_casuale(int k)
+{
+	return rand() % ((2 * k) + 1) + (-k);
+}
+
+void azzera_polinomiale(int k, int n, int x[])
+{
+	int soluzioni = 0;
+	int somma = 0;
 	int appoggio;
-	for(int i=0; i<n; i++)  somma+=x[i]*pow(2,i);
-	
-	for(int i=0; i<n;i++) {
-		appoggio= -somma + x[i]*pow(2,i);
-		
-	if((appoggio % (int)pow(2,i))==0 && (appoggio / pow(2,i))<=k && (appoggio / pow(2,i)) >=-k) soluzioni++;
-  
-	 
+	for (int i = 0; i < n; i++)
+		somma += x[i] * pow(2, i);
+
+	for (int i = 0; i < n; i++)
+	{
+		appoggio = -somma + x[i] * pow(2, i);
+
+		if ((appoggio % (int)pow(2, i)) == 0 && (appoggio / pow(2, i)) <= k && (appoggio / pow(2, i)) >= -k)
+			soluzioni++;
+	}
+
+	cout << endl
+			 << soluzioni;
 }
-	cout << endl << soluzioni;
-}	
-	
-	
-	
-	
-	
-	
